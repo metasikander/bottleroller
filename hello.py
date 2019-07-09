90f41221a2443d619032568241069aff8de0d66a
Sikander <sikander@lhote.me>
2019-07-09 16:50:37 +0200

    Init

--- hello.py
+++ hello.py
@@ -1,7 +1,12 @@
 from bottle import route, run
+import Dice-Roller
 
 @route('/hello')
 def hello():
     return "Hello World!"
+
+@route('/roll')
+def roll():
+    return
 
 run(host='localhost', port=8080, debug=True)
